1560e6fee71a71c7484dc4fd2389f7c5b41e5834
Ice3yes <hakantekir2002@outlook.com>
2022-06-06 14:52:06 +0300

    Commit

--- src/main/java/com/hakan/project/test.java
+++ src/main/java/com/hakan/project/test.java
@@ -11,7 +11,7 @@ public class test extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("menu-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 635);
+        Scene scene = new Scene(fxmlLoader.load(), 300, 400);
         stage.setTitle("Menu");
         stage.setResizable(false);
         stage.setScene(scene);
